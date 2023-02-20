d67090acefacd2b14166d89ea9ed2a263f7a2c0d
Abdalrahman <abdalrahmandaas963@gmail.com>
2023-02-20 21:06:23 +0300

    first

--- src/components/asks/Asks.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./asks.css";
-
-function Asks() {
-  return <div className="welcome asks">Asks</div>;
-}
-
-export default Asks;
--- src/components/button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./button.css";
-
-function Button({ title }) {
-  return <div className="btn">{title}</div>;
-}
-
-export default Button;
